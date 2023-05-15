a6dfffe3fe7f8103a5d2e3ad12042c61892c3b0e
KimTechPreneur <cyrilkimbi@gmail.com>
2023-05-15 15:17:19 +0100

    first commit

---
+++ kim/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+reactDom.render(<APP/> , document.getElementById('root')); //THIS LINE INDICATE THAT
+// ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE
